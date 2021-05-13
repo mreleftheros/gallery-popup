442b52d7f1028065d372782e8d6b751a48e2e356
eleftheros <eleftheros_all@protonmail.com>
2021-05-13 09:21:45 +0300

    Add closeImg event

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,25 @@ const modal = document.querySelector(".modal-container");
 const modalImg = document.querySelector(".modal-img");
 const closeModal = document.querySelector(".close-modal");
 
-// gallery click event
+// events
 gallery.addEventListener("click", openImg);
+closeModal.addEventListener("click", closeImg);
 
 //function to open an image from gallery
 function openImg(e) {
   if(e.target.tagName === "IMG") {
+    //change src
     let src = e.target.attributes[0].nodeValue;
+
+    //update modalImg src attribute
     modalImg.src = src;
     
     //add class open to modal
     modal.classList.add("open");
   }
+}
+
+//function to close modal
+function closeImg() {
+  modal.classList.remove("open");
 }
